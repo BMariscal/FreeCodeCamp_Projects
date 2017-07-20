3c4fcad1659ee74d884cc57f51ad499bc636c61d
Briceida Mariscal <BMariscal@users.noreply.github.com>
2017-07-19 23:48:35 -0700

    added comments

--- tic-tac-toe/js/index.js
+++ tic-tac-toe/js/index.js
@@ -9,30 +9,31 @@ let comptoken;
 let compcall;
 let currentPlayer;
 
+// choose number of players playing
 function players(x) {
   player = x;
- document.getElementById("selectToken").style.display = "block";
-
+  document.getElementById("selectToken").style.display = "block";
 }
+//choose your token
 function token(sym) {
   symb = sym;
-   document.getElementById("startGameButton").style.display = "block";
+  document.getElementById("startGameButton").style.display = "block";
 }
 
+// click on start button
 function start() {
-
   document.getElementById("top-div").style.display = "none";
   document.getElementById("controlBox").style.display = "block";
   document.getElementById("tic-tac-toe-box").style.display = "block";
   if (player === 1) {
     let starts = Math.floor(Math.random() * 2) + 1;
     playSolo(starts);
-  }else if (player === 2){
-        document.getElementById("Winner").innerHTML = `${symb}'s  turn`;
+  } else if (player === 2) {
+    document.getElementById("Winner").innerHTML = `${symb}'s  turn`;
   }
-
 }
 
+//the view is populated by the items in the gameArr array
 function gameView(arr) {
   for (let i = 0; i < arr.length; i++) {
     let num = i;
@@ -47,6 +48,8 @@ function gameView(arr) {
     }
   }
 }
+
+//update view with the winner
 function theWinner(winner) {
   console.log(comptoken + " comptoken");
   console.log("winner " + winner);
@@ -76,7 +79,7 @@ function theWinner(winner) {
       reset(gameArr);
     }, 1000);
   }
-   if (clickCount === 9 && !checkWinner(gameArr) && player === 1) {
+  if (clickCount === 9 && !checkWinner(gameArr) && player === 1) {
     document.getElementById("Winner").innerHTML = "Cat's game";
     clearTimeout(compcall);
     setTimeout(function() {
@@ -85,19 +88,19 @@ function theWinner(winner) {
   }
 }
 
-function toggleToken(symb){
-  if( symb === "X"){
-      document.getElementById("Winner").innerHTML = "O's turn";
-  }else{
-          document.getElementById("Winner").innerHTML = "X's turn";
+//updates view indicating the current player's token
+function toggleToken(symb) {
+  if (symb === "X") {
+    document.getElementById("Winner").innerHTML = "O's turn";
+  } else {
+    document.getElementById("Winner").innerHTML = "X's turn";
   }
 }
 
-
+//called from DOM when tic-tac-toe cell is clicked; num is the id of the cell
 function clicked(num) {
-
   if (player === 2) {
-    toggleToken(symb)
+    toggleToken(symb);
     clickCount++;
     let currentToken = newToken(symb);
     gameArr.splice(num, 1, currentToken);
@@ -105,7 +108,6 @@ function clicked(num) {
     let winner = checkWinner(gameArr);
     theWinner(winner);
   } else if (player === 1) {
-
     clickCount++;
     let currentToken = newToken(symb);
     gameArr.splice(num, 1, currentToken);
@@ -123,6 +125,7 @@ function clicked(num) {
   }
 }
 
+//initializes game when playing against computer
 function playSolo(starts) {
   let yourToken = symb;
   let computerToken = symb === "X" ? "O" : "X";
@@ -135,6 +138,7 @@ function playSolo(starts) {
   }
 }
 
+//computer's actions when playing against the computer
 function computer() {
   document.getElementById("Winner").innerHTML = "Computer";
   comptoken = symb === "X" ? "O" : "X";
@@ -153,6 +157,7 @@ function computer() {
   }
 }
 
+//toggles token for two player game
 function newToken(symb) {
   if (symb === "X") {
     token("O");
@@ -163,10 +168,12 @@ function newToken(symb) {
   }
 }
 
+//restarts game from the top/beginning
 function restart() {
   window.location.reload(true);
 }
 
+//Checks for winner; returns winning token of winner or null if no winner
 function checkWinner(arr) {
   const winner = [
     [0, 1, 2],
@@ -189,25 +196,25 @@ function checkWinner(arr) {
   return null;
 }
 
+//resets game
 function reset(arr) {
   gameArr = ["", "", "", "", "", "", "", "", ""];
   clickCount = 0;
 
   for (let i = 0; i < arr.length; i++) {
-
     let index = i;
     function doStuff(index, arr) {
       document.getElementById(`btn${index}`).innerHTML = "";
       document.getElementById(`btn${index}`).disabled = false;
- setTimeout(function() {
-    document.getElementById("Winner").innerHTML = "";
+      setTimeout(function() {
+        document.getElementById("Winner").innerHTML = "";
       }, 1000);
-      if (player === 2){
- setTimeout(function() {
-    document.getElementById("Winner").innerHTML = "You Start " + symb; ;
-      }, 1000);}
+      if (player === 2) {
+        setTimeout(function() {
+          document.getElementById("Winner").innerHTML = "You Start " + symb;
+        }, 1000);
+      }
     }
     doStuff(index, gameArr);
   }
-
 }
